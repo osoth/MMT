6ca418ac5c8e309f0e70592443273a73d892e49e
soth1 <soth@uni-potsdam.de>
2024-01-18 13:34:21 +0100

    blablab

--- main.py
+++ main.py
@@ -2,24 +2,39 @@ from PIL import Image
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
-app = Ursina()
 
-# Load the image
-img = Image.open('Hamburger.jpg')
-pixels = img.load()
+class CustomController(FirstPersonController):
+	def update(self):
+		super().update()
 
-# Get the size of the image
-width, height = img.size
+		if held_keys['space']:
+			self.y += 2 * time.dt  # move up
+		if held_keys['left shift']:
+			self.y -= 2 * time.dt  # move down
 
-# Create a 3D structure of colored cubes
-for y in range(height):
-	for x in range(width):
-		r, g, b = pixels[x, y]
-		color = rgb(r, g, b)  # Use the rgb function to create a color
-		cube = Entity(model='cube', color=color, scale=(1, 1, 1), position=(x - width / 2, 0, y - height / 2))
-		cube.collider = BoxCollider(cube)
-# Add a first-person controller
-player = FirstPersonController()
-player.y = 10
 
-app.run()
+if __name__ == "__main__":
+	app = Ursina()
+
+	# Load the image
+	img = Image.open('rickastley.jpg')
+	pixels = img.load()
+
+	# Get the size of the image
+	width, height = img.size
+
+	# Create a 3D structure of colored cubes
+	for y in range(height):
+		for x in range(width):
+			r, g, b = pixels[x, y]
+			color = rgb(r, g, b)  # Use the rgb function to create a color
+			cube = Entity(model='cube', color=color, scale=(1, 1, 1), position=(x - width / 2, 0, y - height / 2))
+			cube.collider = BoxCollider(cube)
+
+	# Add a custom first-person controller
+	player = CustomController()
+	player.y = 10
+	player.gravity = 0
+	player.speed = 10  # increase movement speed
+
+	app.run()
