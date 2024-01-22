21bc231a1c92858bd36d889a5e45e7c930d10f19
soth1 <soth@uni-potsdam.de>
2024-01-22 17:04:40 +0100

    deine nudda

--- hovering.py
+++ hovering.py
@@ -1,18 +1,18 @@
 from ursina import *
-from ursina import time
+from ursina.prefabs.slider import Slider
 import dct
 
 app = Ursina()
-# Erstellen Sie eine neue Szene
+
+# Create a new scene
 scene = Entity()
-clock_1 = 0.0
 i = 0
 
-# Erstellen Sie eine neue Ebene
+# Create a new plane
 parent_plane = Entity()
 plane = Entity(parent=parent_plane, model='plane', texture='Bilder/BackTransformed1.jpg')
 
-# Zentrieren Sie die Ebene in der Mitte der Szene
+# Center the plane in the middle of the scene
 plane.x = 0
 plane.y = 0
 plane.z = 0
@@ -20,25 +20,19 @@ plane.rotation_z = 0
 plane.rotation_x = +90
 plane.rotation_y = +160
 Trans = dct.dct2('rickastley.jpg')
-for k in range(0,64):
-    Trans = dct.koeffizientenAnpassung(Trans,k)
-    dct.Idct2(Trans,k)
+
+# Create a slider
+slider = Slider(min=0, max=64, default_value=0, dynamic=True, position=(-0.25, -0.45))
 
 
-def input(key, Trans=Trans):
-	global i
-	if (key == 'space'):
-		plane.texture = load_texture('Bilder/BackTransformed'+str(i)+'.jpg')
-		invoke(setattr, plane, 'y', plane.y, delay=.25)
-		i += 1
-		print(i)
-
-
-# Schreiben Sie den Code, um die Ebene um ihre eigene Achse zu drehen
 def update():
-	pass
-
+	global i
+	global Trans
+	i = int(slider.value)
+	Trans = dct.koeffizientenAnpassung(Trans, i)
+	dct.Idct2(Trans, i)
+	plane.texture = load_texture('Bilder/BackTransformed' + str(i) + '.jpg')
+	invoke(setattr, plane, 'y', plane.y, delay=.25)
 
 EditorCamera()
-# Starten Sie die Anwendung
 app.run()
