32254e2a3f4ad8fd45cde789eaf87810e5dfb7f5
k3llby <isheim@uni-potsdam.de>
2024-02-08 20:08:00 +0100

    added popups

--- hovering2.py
+++ hovering2.py
@@ -110,16 +110,36 @@ if __name__ == "__main__":
 	plane3_button = Button(position=(-0.6, -0.25), scale=(0.5, 0.2), parent=options_menu, text='Toggle Plane3 (Currently: Visible)', on_click=toggle_plane3)
 	#button1 = Button(text='Button 1', color=color.azure, scale=(0.1, 0.05), position=(-0.2, 0.2), on_click=on_button1_click)
 
-	popupText = Text(text="", position=(0.4,0.4), color=color.white, background=True, visible=False, size=0.02)
+	popupText = Text(text="", position=(0.3,0.4), color=color.white, background=True, visible=False, size=0.02, line_height=1.4)
 
-	popup1 = "To do"
-	popup2 = "To do"
-	popup3 = "To do"
-	popup4 = "To do"
+	popup1 = 'Komprimiertes Bild. Insbesondere bei hoher Kompressionsrate wird die 8x8 Segmentierung durch die scharfen Kanten deutlich. Bei einem hochauflösenden Bild sind die Kanten dass, was wir als "Pixelig" wahrnehmen. '
+	popup2 = "Jeder 8x8 Block des Bildes wird als Linearkombination der Quantisierungstabelle dargestellt. Die Ebene stellt dabei den Wert der Linearfaktoren graphisch dar (1 = Weiß, 0 = Schwarz). Dabei treten hohe Koeffizienten vor allem in den niedrigen Frequenzen, dass heißt in der linken oberen Ecke auf. Da lediglich die Koeffizienten ungleich 0 gespeichert werden müssen um das Bild rekonstruieren zu können, ist der Speicherbedarf des Zielbildes kleiner als beim Original. Die mögliche Kompressionsrate für ein zufriedenstellendes Resultat ist dabei vom Originalbild abhängig. Grundsätzlich lassen sich Bilder mit großen Flächen (z.B. einfarbige Wand) stärker kompremieren, als unruhige Bilder (z.B. weißes Rauschen), da weniger Linearfaktoren benötigt werden (größere Unterschiede der Koeffizienten). Über den Slider lassen sich die Koeffizienten stufenweise, von den Frequenzen absteigend, null setzen. "
+	popup3 = "Originalbild als Graustufenbild. Das Bild wird in 8x8 Blöcke segmentiert, so dass die DCT angewendet werden kann. Da es sich um ein Bild handelt wird der zweidimensionale DCT-2 angewandt. "
+	popup4 = "Quantisierungstabelle: Unabhängig vom Bild wendet jede DCT-2 diese Qunatisierungstabelle an. Die 8x8 Einträge bestehen aus je 8x8 Pixeln und representieren vertikale und horizontale Cosinus Schwingungen in unterschiedlichen Frequenzen. Die Einträge berechnen sich durch die Multiplikation zweier, zueinander senkrechter cos-Funktionen auf dem Intervall 0 bis Pi. Der Farbwert jedes Subpixels ist dabei der Wert der Funktion an der mittleren Position des Feldes, wobei 1 als Weiß und -1 als Schwarz interpretiert werden. Die Frequenzen nehmen von Links oben nach rechts unten zu. Der Block in der linken oberen Ecke wiederspiegelt den Gleichanteil und somit die mittlere Helligkeit des 8x8 Blocks des Bildes. Jeder 8x8 Schwarz-Weiß-Block kann als Linearkombination dieser 64 Einträge dargestellt werden. "
 
+	# Add new line to popups after every 4 words
+	def add_new_line(popup):
+		popup = popup.split()
+		n = 35
+		newpopup = ""
+		while popup:
+			tmp = ""
+			while len(tmp) < n:
+				if not popup:
+					break
+				tmp += popup.pop(0) + " "
+			newpopup += tmp + "\n"
+		return  newpopup
+	
+	popup1 = add_new_line(popup1)
+	popup2 = add_new_line(popup2)
+	popup3 = add_new_line(popup3)
+	popup4 = add_new_line(popup4)
+
+	
 	popup_button = Button(position=(0.5, 0.01, 0.5), rotation=(90,180,0), scale=(0.15, 0.15), color=color.turquoise, parent=plane, on_click=Func(popup, popup1))
-	popup_button2 = Button(position=(0.5, 0.01, 0.5), rotation=(90,180,0), scale=(0.15, 0.15), color=color.turquoise, parent=plane2, on_click=Func(popup, popup2))
-	popup_button3 = Button(position=(0.5, 0.01, 0.5), rotation=(90,180,0), scale=(0.15, 0.15), color=color.turquoise, parent=plane3, on_click=Func(popup, popup3))
+	popup_button2 = Button(position=(0.5, 0.01, 0.5), rotation=(90,180,0), scale=(0.15, 0.15), color=color.turquoise, parent=plane2, on_click=Func(popup, popup3))
+	popup_button3 = Button(position=(0.5, 0.01, 0.5), rotation=(90,180,0), scale=(0.15, 0.15), color=color.turquoise, parent=plane3, on_click=Func(popup, popup2))
 	popup_button4 = Button(position=(0.5, 0.01, 0.5), rotation=(90,180,0), scale=(0.15, 0.15), color=color.turquoise, parent=plane4, on_click=Func(popup, popup4))
 
 	# Create start screen
